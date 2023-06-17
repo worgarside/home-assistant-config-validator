77a32c3026744b77e47aaca9b77f7f9f1a63c25c
Will Garside <worgarside@gmail.com>
2023-06-17 11:31:30 +0100

    Start work on PCH

---
+++ home_assistant_config_validator/constants.py
@@ -0,0 +1,148 @@
+"""Constants for the configuration validator."""
+
+# pylint: disable=wrong-import-position
+# mypy: disable-error-code="import"
+from __future__ import annotations
+
+import re
+import sys
+from pathlib import Path
+from unittest.mock import MagicMock
+
+from homeassistant.components.binary_sensor import (
+    PLATFORM_SCHEMA as BINARY_SENSOR_SCHEMA,
+)
+from homeassistant.components.bluetooth_le_tracker.device_tracker import (
+    PLATFORM_SCHEMA as BLUETOOTH_LE_TRACKER_SCHEMA,
+)
+from homeassistant.components.device_tracker import (
+    PLATFORM_SCHEMA as DEVICE_TRACKER_SCHEMA,
+)
+from homeassistant.components.google_maps.device_tracker import (
+    PLATFORM_SCHEMA as GOOGLE_MAPS_SCHEMA,
+)
+from homeassistant.components.history_stats.sensor import (
+    PLATFORM_SCHEMA as SENSOR_HISTORY_STATS_SCHEMA,
+)
+from homeassistant.components.input_boolean import InputBooleanStorageCollection
+from homeassistant.components.input_button import InputButtonStorageCollection
+from homeassistant.components.input_datetime import DateTimeStorageCollection
+from homeassistant.components.input_number import NumberStorageCollection
+from homeassistant.components.input_select import InputSelectStorageCollection
+from homeassistant.components.input_text import InputTextStorageCollection
+from homeassistant.components.london_air.sensor import (
+    PLATFORM_SCHEMA as SENSOR_LONDON_AIR_SCHEMA,
+)
+from homeassistant.components.mqtt.binary_sensor import (
+    PLATFORM_SCHEMA_MODERN as MQTT_BINARY_SENSOR_SCHEMA,
+)
+from homeassistant.components.mqtt.sensor import (
+    PLATFORM_SCHEMA_MODERN as MQTT_SENSOR_SCHEMA,
+)
+from homeassistant.components.ping.binary_sensor import PLATFORM_SCHEMA as PING_SCHEMA
+from homeassistant.components.rest.sensor import PLATFORM_SCHEMA as SENSOR_REST_SCHEMA
+from homeassistant.components.rest_command import CONFIG_SCHEMA as REST_COMMAND_SCHEMA
+from homeassistant.components.script.config import SCRIPT_ENTITY_SCHEMA as SCRIPT_SCHEMA
+from homeassistant.components.systemmonitor.sensor import (
+    PLATFORM_SCHEMA as SENSOR_SYSTEMMONITOR_SCHEMA,
+)
+from homeassistant.components.template.binary_sensor import (
+    BINARY_SENSOR_SCHEMA as TEMPLATE_BINARY_SENSOR_SCHEMA,
+)
+from homeassistant.components.template.cover import COVER_SCHEMA
+from homeassistant.components.template.sensor import (
+    SENSOR_SCHEMA as TEMPLATE_SENSOR_SCHEMA,
+)
+from homeassistant.components.universal.media_player import (
+    PLATFORM_SCHEMA as UNIVERSAL_MEDIA_PLAYER_SCHEMA,
+)
+
+# This import order fixes an issue with `websocket_api.websocket_command` not being
+# loaded correctly; not sure why
+from homeassistant.components.automation.config import (  # isort:skip
+    PLATFORM_SCHEMA as AUTOMATION_SCHEMA,
+)
+
+# Mock out packages that are not installable in a CI environment and would otherwise
+# cause the custom component to fail to load
+for package in (
+    "bluetooth",
+    "bluetooth._bluetooth",
+):
+    sys.modules[package] = MagicMock()
+
+
+from homeassistant.components.bluetooth_tracker.device_tracker import (  # noqa: E402
+    PLATFORM_SCHEMA as BLUETOOTH_TRACKER_SCHEMA,
+)
+
+REPO_PATH = Path(__file__).parents[2]
+
+# Allow custom components to be loaded
+sys.path.append(
+    custom_components_path_str := (REPO_PATH / "custom_components").as_posix()
+)
+
+try:
+    from feedparser.sensor import PLATFORM_SCHEMA as SENSOR_FEEDPARSER_SCHEMA
+    from var import CONFIG_SCHEMA as VAR_SCHEMA
+except ImportError as exc:
+    if not re.fullmatch(
+        r"^No module named (?:'|\")([a-z_]+[a-z0-9_-]*)(?:'|\")$",
+        str(exc),
+        flags=re.IGNORECASE,
+    ):
+        raise
+
+    raise FileNotFoundError(
+        f"Directory `{custom_components_path_str}` not found; please create it"
+        " and try again"
+    ) from exc
+
+VALIDATOR_CONFIGS = Path(__file__).parent / "validator_configs.json"
+ENTITIES_DIR = REPO_PATH / "entities"
+INTEGS_DIR = REPO_PATH / "integrations"
+
+DOMAIN_SCHEMA_MAP = {
+    "automation": AUTOMATION_SCHEMA,
+    "binary_sensor": BINARY_SENSOR_SCHEMA,
+    "binary_sensor.ping": PING_SCHEMA,
+    "cover": COVER_SCHEMA,
+    "device_tracker": DEVICE_TRACKER_SCHEMA,
+    "device_tracker.bluetooth_le_tracker": BLUETOOTH_LE_TRACKER_SCHEMA,
+    "device_tracker.bluetooth_tracker": BLUETOOTH_TRACKER_SCHEMA,
+    "device_tracker.google_maps": GOOGLE_MAPS_SCHEMA,
+    "input_boolean": InputBooleanStorageCollection.CREATE_UPDATE_SCHEMA,
+    "input_button": InputButtonStorageCollection.CREATE_UPDATE_SCHEMA,
+    "input_datetime": DateTimeStorageCollection.CREATE_UPDATE_SCHEMA,
+    "input_number": NumberStorageCollection.SCHEMA,
+    "input_select": InputSelectStorageCollection.CREATE_UPDATE_SCHEMA,
+    "input_text": InputTextStorageCollection.CREATE_UPDATE_SCHEMA,
+    "media_player.universal": UNIVERSAL_MEDIA_PLAYER_SCHEMA,
+    "mqtt.binary_sensor": MQTT_BINARY_SENSOR_SCHEMA,
+    "mqtt.sensor": MQTT_SENSOR_SCHEMA,
+    "rest_command": REST_COMMAND_SCHEMA,
+    "script": SCRIPT_SCHEMA,
+    "sensor.history_stats": SENSOR_HISTORY_STATS_SCHEMA,
+    "sensor.feedparser": SENSOR_FEEDPARSER_SCHEMA,
+    "sensor.london_air": SENSOR_LONDON_AIR_SCHEMA,
+    "sensor.rest": SENSOR_REST_SCHEMA,
+    "sensor.systemmonitor": SENSOR_SYSTEMMONITOR_SCHEMA,
+    "template.binary_sensor": TEMPLATE_BINARY_SENSOR_SCHEMA,
+    "template.sensor": TEMPLATE_SENSOR_SCHEMA,
+    "var": VAR_SCHEMA,
+}
+
+SCRIPT_NAMES = [
+    f"script.{script_file.stem}"
+    for script_file in (ENTITIES_DIR / "script").rglob("*.yaml")
+]
+
+SCRIPT_NAME_PATTERN = re.compile(r"script\.(?P<script_name>.*)")
+
+SCRIPT_SERVICES = [
+    "script.reload",
+    "script.toggle",
+    "script.turn_off",
+    "script.turn_on",
+]
---
+++ home_assistant_config_validator/ha_yaml_loader.py
@@ -0,0 +1,337 @@
+"""Functionality for loading Home Assistant YAML files."""
+
+from __future__ import annotations
+
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Any, ClassVar, cast
+
+from attr import dataclass
+from constants import INTEGS_DIR, REPO_PATH
+from wg_utilities.functions.json import JSONObj, JSONVal
+from yaml import SafeLoader, ScalarNode, load
+
+
+@dataclass
+class _CustomTag:
+    TAG: ClassVar[str]
+
+    @classmethod
+    def construct(
+        cls, loader: SafeLoader, node: ScalarNode, **kwargs: dict[str, Any]
+    ) -> _CustomTag:
+        """Construct a custom tag from a YAML node.
+
+        Args:
+            loader (SafeLoader): The YAML loader
+            node (ScalarNode): The YAML node to construct from
+            **kwargs (dict[str, Any]): Additional keyword arguments to pass to the
+                constructor
+
+        Returns:
+            _CustomTag: The constructed custom tag
+        """
+
+        # pylint: disable=too-many-function-args
+        return cls(loader.construct_scalar(node), **kwargs)  # type: ignore[call-arg]
+
+
+class _CustomTagWithPath(_CustomTag):
+    path: str
+
+
+@dataclass
+class IncludeDirList(_CustomTagWithPath):
+    """Return the content of a directory as a list.
+
+    https://www.home-assistant.io/docs/configuration/splitting_configuration/#advanced-usage
+
+    !include_dir_list will return the content of a directory as a list with each file
+    content being an entry in the list. The list entries are ordered based on the
+    alphanumeric ordering of the names of the files.
+    """
+
+    TAG: ClassVar[str] = "!include_dir_list"
+    path: str
+
+    def load_data(self, relative_to: Path = INTEGS_DIR) -> list[JSONObj]:
+        """Load the data from the directory.
+
+        Args:
+            relative_to (Path, optional): The path to load the data relative to.
+                Defaults to INTEGS_DIR.
+
+        Returns:
+            list[JSONObj]:the content of a directory as a list with each file content
+                being an entry in the list
+
+        Raises:
+            TypeError: If any of the files in the directory do not contain a dictionary
+        """
+        data: list[JSONObj] = []
+
+        for file in sorted((relative_to / self.path).rglob("*.yaml")):
+            file_content = load_yaml(file)
+
+            if isinstance(file_content, dict):
+                data.append(file_content)
+            else:
+                raise TypeError(
+                    f"File {file} contains a {type(file_content)}, but"
+                    "`!include_dir_list` expects each file to contain a dictionary"
+                )
+
+        return data
+
+
+@dataclass
+class IncludeDirMergeList(_CustomTagWithPath):
+    """Return the content of a directory as a list.
+
+    https://www.home-assistant.io/docs/configuration/splitting_configuration/#advanced-usage
+
+    !include_dir_merge_list will return the content of a directory as a list by merging
+    all files (which should contain a list) into 1 big list.
+    """
+
+    TAG: ClassVar[str] = "!include_dir_merge_list"
+    path: str
+
+    def load_data(self, relative_to: Path = INTEGS_DIR) -> list[JSONObj]:
+        """Load the data from the directory.
+
+        Args:
+            relative_to (Path, optional): The path to load the data relative to.
+                Defaults to INTEGS_DIR.
+
+        Returns:
+            list[JSONObj]: The content of a directory as a list by merging all files
+                (which should contain a list) into 1 big list.
+
+        Raises:
+            TypeError: If any of the files in the directory do not contain a list
+        """
+        data: list[JSONObj] = []
+
+        for file in sorted((relative_to / self.path).rglob("*.yaml")):
+            file_content = load_yaml(file)
+
+            if isinstance(file_content, list):
+                data.extend(file_content)
+            else:
+                raise TypeError(
+                    f"File {file} contains a {type(file_content)}, but"
+                    "`!include_dir_merge_list` expects each file to contain a list"
+                )
+
+        return data
+
+
+@dataclass
+class IncludeDirMergeNamed(_CustomTagWithPath):
+    """Return the content of a directory as a dictionary.
+
+    https://www.home-assistant.io/docs/configuration/splitting_configuration/#advanced-usage
+
+    !include_dir_merge_named will return the content of a directory as a dictionary by
+    loading each file and merging it into 1 big dictionary.
+    """
+
+    TAG: ClassVar[str] = "!include_dir_merge_named"
+    path: str
+
+    def load_data(self, relative_to: Path = INTEGS_DIR) -> JSONObj:
+        """Load the data from the directory.
+
+        Args:
+            relative_to (Path, optional): The path to load the data relative to.
+                Defaults to INTEGS_DIR.
+
+        Returns:
+            JSONObj: The content of a directory as a dictionary by merging all files
+                (which should contain a dictionary) into 1 big dictionary.
+
+        Raises:
+            TypeError: If any of the files in the directory do not contain a dictionary
+        """
+        data = {}
+
+        for file in sorted((relative_to / self.path).rglob("*.yaml")):
+            print("    Loading", file)
+            file_content = load_yaml(file)
+
+            if isinstance(file_content, dict):
+                data.update(file_content)
+            else:
+                raise TypeError(
+                    f"File {file.as_posix()} contains a {type(file_content)}, but"
+                    "`!include_dir_merge_named` expects each file to contain a"
+                    " dictionary"
+                )
+
+        return data
+
+
+@dataclass
+class IncludeDirNamed(_CustomTagWithPath):
+    """Return the content of a directory as a dictionary.
+
+    https://www.home-assistant.io/docs/configuration/splitting_configuration/#advanced-usage
+
+    !include_dir_named will return the content of a directory as a dictionary which
+    maps filename => content of file.
+    """
+
+    TAG: ClassVar[str] = "!include_dir_named"
+    path: str
+
+    def load_data(self, relative_to: Path = INTEGS_DIR) -> JSONObj:
+        """Load the data from the directory.
+
+        Args:
+            relative_to (Path, optional): The path to load the data relative to.
+                Defaults to INTEGS_DIR.
+
+        Returns:
+            JSONObj: The content of a directory as a dictionary with each file content
+                being a key in the dictionary. The key is the filename without the
+                extension.
+
+        Raises:
+            TypeError: If any of the files in the directory do not contain a single
+                object
+        """
+        data: JSONObj = {}
+
+        for file in sorted((relative_to / self.path).rglob("*.yaml")):
+            file_content = load_yaml(file)
+
+            if isinstance(file_content, dict):
+                data.update(file_content)
+            else:
+                raise TypeError(
+                    f"File {file.as_posix()} contains a list, but `!include_dir_list`"
+                    " expects each file to contain a single object"
+                )
+
+        return data
+
+
+@dataclass
+class Secret(_CustomTag):
+    """Return the value of a secret.
+
+    https://www.home-assistant.io/docs/configuration/secrets#using-secretsyaml
+    """
+
+    FAKE_SECRETS_PATH: ClassVar[Path] = REPO_PATH / "secrets.fake.yaml"
+    TAG: ClassVar[str] = "!secret"
+
+    secret_id: str
+
+    @classmethod
+    def get_value_callback(
+        cls,
+        secret: Secret,
+        *,
+        dict_key: str | None = None,
+        list_index: int | None = None,
+    ) -> JSONVal:
+        """Get a substitute value for a secret from the ID."""
+
+        _ = dict_key, list_index
+
+        return secret.get_fake_value()
+
+    def get_fake_value(self, fallback_value: str | None = None) -> JSONVal:
+        """Get a substitute value for a secret.
+
+        Args:
+            fallback_value (str | None, optional): The value to return if the secret
+                is not found. Defaults to None.
+
+        Returns:
+            Any: The substitute value for the secret.
+
+        Raises:
+            TypeError: If the file does not contain a dictionary.
+            ValueError: If the secret is not found in the file and no fallback value
+                is provided.
+        """
+        fake_secrets = load_yaml(self.FAKE_SECRETS_PATH)
+
+        if isinstance(fake_secrets, dict):
+            if (
+                fake_secret := fake_secrets.get(self.secret_id, fallback_value)
+            ) is not None:
+                return fake_secret  # type: ignore[no-any-return]
+        else:
+            raise TypeError(
+                f"File {self.FAKE_SECRETS_PATH.as_posix()} contains a"
+                f" {type(fake_secrets)}, but `!secret` expects it to contain a"
+                " dictionary"
+            )
+
+        raise ValueError(
+            f"Secret {self.secret_id!r} not found in"
+            f" {self.FAKE_SECRETS_PATH.as_posix()!r}"
+        )
+
+
+# pylint: disable=too-few-public-methods
+class HAYamlLoader(SafeLoader):
+    """A YAML loader that supports custom Home Assistant tags."""
+
+
+def load_yaml(path: Path) -> JSONObj | Iterable[JSONVal]:
+    """Load a YAML file.
+
+    Args:
+        path (Path): The path to the YAML file.
+
+    Returns:
+        JSONObj | Iterable[JSONVal]: The content of the YAML file as a JSON object or
+            iterable of JSON values.
+    """
+    return cast(
+        JSONObj | Iterable[JSONVal],
+        load(path.read_text(), Loader=HAYamlLoader),
+    )
+
+
+def subclasses_recursive(cls: type[_CustomTag]) -> list[type[_CustomTag]]:
+    """Get all subclasses of a class recursively.
+
+    Args:
+        cls (type[_CustomTag]): The class to get the subclasses of.
+
+    Returns:
+        list[type[_CustomTag]]: A list of all subclasses of the class.
+    """
+    indirect = []
+    for subclass in (direct := cls.__subclasses__()):
+        indirect.extend(subclasses_recursive(subclass))
+    return direct + indirect
+
+
+def add_custom_tags_to_loader(loader: type[SafeLoader]) -> None:
+    """Add all custom tags to a YAML loader.
+
+    Args:
+        loader (type[SafeLoader]): The YAML loader to add the custom tags to.
+    """
+    for tag_class in subclasses_recursive(_CustomTag):
+        if not tag_class.__name__.startswith("_"):
+            loader.add_constructor(tag_class.TAG, tag_class.construct)
+
+
+add_custom_tags_to_loader(HAYamlLoader)
+
+__all__ = [
+    "IncludeDirList",
+    "IncludeDirMergeList",
+    "IncludeDirMergeNamed",
+    "IncludeDirNamed",
+    "Secret",
+    "load_yaml",
+]
