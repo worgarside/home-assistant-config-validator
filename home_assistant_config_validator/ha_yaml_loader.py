939f3def887c9bd2df84bfc56b1f9546f373bcd6
worgarside-dev <worgarside.dev@gmail.com>
2023-06-19 14:36:16 +0100

    Update to Python 3.11 (#8)
    * Update Python version spec for PCH
    
    * Update to Python 3.11
    
    * Remove HA stubs from mypy PCH
    
    * Remove `hasspi-sync` semver label check
    
    ---------
    
    Co-authored-by: Will Garside <william.garside@sigtech.com>
    Co-authored-by: Will Garside <worgarside@gmail.com>

--- home_assistant_config_validator/ha_yaml_loader.py
+++ home_assistant_config_validator/ha_yaml_loader.py
@@ -3,10 +3,10 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, ClassVar, cast
 
-from attr import dataclass
 from wg_utilities.functions.json import JSONObj, JSONVal
 from yaml import SafeLoader, ScalarNode, load
 
